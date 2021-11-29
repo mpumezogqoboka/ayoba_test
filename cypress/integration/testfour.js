57f272afb97e6b101fc18ee9d56bbd9402ee6fe7
unknown <mpumezogqoboka@gmail.com>
2021-11-29 18:42:37 +0200

    added new files

---
+++ cypress/integration/testfour.js
@@ -0,0 +1,19 @@
+/// <reference types='cypress'/>
+
+
+it('Ayoba contact us page', () => {
+
+    const imageFile = 'mezzo.jpg';
+
+    cy.visit('https://www.ayoba.me/contact-en/')
+    cy.get('#zen_name').type('Abongile Gqoboka')
+    cy.get('#zen_email').type('abongile@gmail.com')
+    cy.get('#\\33 60002776780').select('I need help using the Ayoba app').should('have.value', 'help')
+    cy.get('#phoneDialingCodes').select('(+27) South Africa').should('have.value', '27')
+    cy.get('#zen_desc').type('I have been trying to use the Ayoba app and I am struggling can you please assist me and make understand how the app works and how to use the app. thank you. ')
+    cy.fixture('example.json').then( (filecontent) => {
+
+        cy.get('#zen_files').attachFile(imageFile);
+    })
+    cy.get('#zrf_submit').click()
+})
